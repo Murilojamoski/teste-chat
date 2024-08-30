dac9d6d79090b2e81a62eb390a77664c536d9672
Murilojamoski <Mugames2002@gmail.com>
2024-08-30 20:49:24 -0300

    criado backend

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const { WebSocketServer } = require("ws") //puxa nossa biblioteca web
+const dotenv = require("dotenv") // puxa a nossa variavel ambiente(serve para as portas)
+
+dotenv.config() // permite usar a variavel do .env
+
+const wss = new WebSocketServer({port: process.env.PORT || 8080}) //liga o servidor
+
+wss.on("connection", (ws)=>{  //quando alguem conectar 
+    ws.on("error", console.error) // caso tenha erro ele manda pro console
+
+    ws.on("message", (data)=>{ // quando alguem manda mensagem
+        wss.clients.forEach((clients) => clients.send(data.toString())) // manda mensagem para todos os clientes (E converte pra string)
+    })
+
+    console.log("conecto familia")
+})
